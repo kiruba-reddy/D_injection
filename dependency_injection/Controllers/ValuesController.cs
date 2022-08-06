aee5dfedf3479d7b1b85cf9eb0a73219ebf49e40
kiruba-reddy <vkirubanandan007@gmail.com>
2022-08-06 10:15:06 +0530

    tested DI

---
+++ dependency_injection/Controllers/ValuesController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using dependency_injection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace dependency_injection.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            DataTable data = new DataTable();
+            string sql = _configuration.GetConnectionString("staff_conn");
+            MySqlDataReader read;
+            //List<staff> data_list = new List<staff>();
+            using (MySqlConnection conn = new MySqlConnection(sql))
+            {
+                conn.Open();
+                using (MySqlCommand command = new MySqlCommand("dis_staff", conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    read = command.ExecuteReader();
+                    data.Load(read);
+                    read.Close();
+                    conn.Close();
+                }
+            }
+            return new JsonResult(data);
+        }
+        [HttpPost]
+        public string Post(staff st)
+        {
+            DataTable data = new DataTable();
+            string sql = _configuration.GetConnectionString("staff_conn");
+            MySqlDataReader read;
+            using(MySqlConnection conn=new MySqlConnection(sql))
+            {
+                conn.Open();
+                using(MySqlCommand command=new MySqlCommand("in_staff",conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@name", st.Name);
+                    command.Parameters.AddWithValue("@id", st.Id);
+                    command.Parameters.AddWithValue("@department", st.Department);
+                    read = command.ExecuteReader();
+                    data.Load(read);
+                    read.Close();
+                    conn.Close();
+
+                }
+            }
+            return "insrted value";
+        }
+        [HttpPut]
+        public string update(staff st)
+        {
+            DataTable data = new DataTable();
+            MySqlDataReader read;
+            string sql = _configuration.GetConnectionString("staff_conn");
+            using(MySqlConnection conn=new MySqlConnection(sql))
+            {
+                conn.Open();
+                using(MySqlCommand command=new MySqlCommand("up_staff",conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@pid", st.Id);
+                    command.Parameters.AddWithValue("@name", st.Name);
+                    command.Parameters.AddWithValue("@department", st.Department);
+                    read = command.ExecuteReader();
+                    data.Load(read);
+                    read.Close();
+                    conn.Close();
+                }
+            }
+            return "updated successfully";
+        }
+        [HttpDelete]
+        public string Delete(staff st)
+        {
+            DataTable data = new DataTable();
+            MySqlDataReader read;
+            string sql = _configuration.GetConnectionString("staff_conn");
+            using (MySqlConnection conn = new MySqlConnection(sql))
+            {
+                conn.Open();
+                using (MySqlCommand command = new MySqlCommand("del_staff",conn))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@pid", st.Id);
+                    read = command.ExecuteReader();
+                    read.Close();
+                    conn.Close();
+                }
+            }
+            return "Deleted successfully";
+        }
+    }
+}
+
---
+++ dependency_injection/Models/staff.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace dependency_injection.Models
+{
+    public class staff
+    {
+            public string Name { get; set; } = "";
+        public int Id { get; set; } = 0;
+        public string Department { get; set; } = "";
+            
+    }
+}
+
